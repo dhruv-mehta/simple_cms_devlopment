3e17947fe9e555e892ad8d8812fb6e014bcd0029
dhruv-mehta <dhm@narola.email>
2016-06-30 18:28:15 +0530

    change the code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,17 @@
 class ApplicationController < ActionController::Base
+
+
+	def status_tag(boolean , options={})
+		
+		options[:true_text] ||= ''
+		options[:false_text] ||= ''
+
+		if boolean
+			content_tag(:span , options[:true_text], :class => 'status true')
+		else
+		content_tag(:span, options[:false_text], :class => 'status false')	
+		end
+	end
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
